1241093a8443ac87a6001704f62a35e16ec25c38
Sakib Sami <s4kibs4mi@gmail.com>
2014-03-04 14:08:29 +0600

    Create Assignment1_Problem1.java

---
+++ Assignment1_Problem1.java
@@ -0,0 +1,60 @@
+public class Assignment1_Problem1 {
+	private int a;
+	private int b;
+	
+	public A1_P1(int a,int b){
+		this.a = a;
+		this.b = b;
+	}
+	
+	public void setA(int a){
+		this.a = a;
+	}
+	
+	public void setB(int b){
+		this.b = b;
+	}
+	
+	public double getA(){
+		return a;
+	}
+	
+	public double getB(){
+		return b;
+	}
+	
+	public int GreaterNumber(){
+		if(a > b) return a;
+		else return b;
+	}
+	
+	public String findRangeA(){
+		if(a >= 0 && a <= 127)
+			return "Range :	0 - 127";
+		else if(a >= 128 && a <= 255)
+			return "Range :	128 - 255";
+		return "256 - infinite";
+	}
+	
+	public String findRangeB(){
+		if(b >= 0 && b <= 127)
+			return "Range :	0 - 127";
+		else if(b >= 128 && b <= 255)
+			return "Range :	128 - 255";
+		return "256 - infinite";
+	}
+	
+	public void asciCharA(){
+		if(a >= 0 && a <= 255)
+			System.out.printf("%c - ASCI\n",a);
+		else System.out.printf("%c - Extended ASCI\n",a%255);
+	}
+	
+	public void asciCharB(){
+		if(b >= 0 && b <= 255)
+			System.out.printf("%c - ASCI\n",b);
+		else System.out.printf("%c - Extended ASCI\n",b%255);
+	}
+	
+	
+}
