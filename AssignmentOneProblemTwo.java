1d4002599f5930eee1dc7f2f60df65a143ade65c
Sakib Sami <s4kibs4mi@gmail.com>
2014-03-09 15:20:43 +0600

    Create AssignmentOneProblemTwo.java

---
+++ AssignmentOneProblemTwo.java
@@ -0,0 +1,55 @@
+public class NamingConventionCheck {
+	private String className;
+	private String methodName;
+	
+	public NamingConventionCheck(){
+		className = null;
+		methodName = null;
+	}
+	
+	public NamingConventionCheck(String className , String methodName){
+		this.className = className;
+		this.methodName = methodName;
+	}
+	
+	public void setClassName(String className){
+		this.className = className;
+	}
+	
+	public void setMethodName(String className){
+		this.methodName = className;
+	}
+	
+	public boolean isClassNameOk(){
+		int n = className.length();
+		
+		if(n == 0)
+			return false;
+		
+		if(Character.isLowerCase(className.charAt(0)))
+			return false;
+		
+		if(className.toLowerCase().equals(className))
+			return false;
+		
+		if(className.toUpperCase().equals(className))
+			return false;
+		
+		if(n > 1 && Character.isUpperCase(className.charAt(n-1)))
+			return false;
+		
+		for(int i = 0; i < n; i++){
+			
+			if(className.charAt(i) == ' ')
+				return false;
+			
+		}
+		return true;
+	}
+	
+	public boolean isMethodOk(){
+		
+		return true;
+	}
+	
+}
