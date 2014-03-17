bdfb08e153adcd7645621f04694e27d0d68db674
Sakib Sami <s4kibs4mi@gmail.com>
2014-03-17 19:06:39 +0600

    Update Complex.java

--- Complex.java
+++ Complex.java
@@ -1,9 +1,9 @@
 public class Complex {
 	private long realNumber;
 	private long imaginaryNumber;
-	
+
 	Complex(){}
-	
+
 	Complex(long realNumber){
 		this.realNumber = realNumber;
 	}
@@ -11,12 +11,23 @@ public class Complex {
 		this.realNumber = realNumber;
 		this.imaginaryNumber = imaginaryNumber;
 	}
-	
+
 	public void setRealNumber(long realNumber){
 		this.realNumber = realNumber;
 	}
 	public void setImaginaryNumber(long imaginaryNumber){
 		this.imaginaryNumber = imaginaryNumber;
+	}
+	
+	public String displayNumber(){
+		return realNumber + " + " + imaginaryNumber + "i";
+	}
+	public long getRealNumber(){
+		return realNumber;
+	}
+	
+	public long getImaginaryNumber(){
+		return imaginaryNumber;
 	}
 	
 	public String addition(Complex numberA){
@@ -25,11 +36,25 @@ public class Complex {
 		result.imaginaryNumber = numberA.imaginaryNumber + imaginaryNumber;
 		return result.realNumber + " + " + result.imaginaryNumber + "i";
 	}
-	
+
 	public String substrction(Complex numberA){
 		Complex result = new Complex();
 		result.realNumber = realNumber - numberA.realNumber;
 		result.imaginaryNumber = imaginaryNumber - numberA.imaginaryNumber;
 		return result.realNumber + " - " + result.imaginaryNumber + "i";
+	}
+	
+	public String multiplication(Complex numberA){
+		Complex result = new Complex();
+		result.realNumber = this.realNumber * numberA.realNumber;
+		result.imaginaryNumber += this.realNumber * numberA.imaginaryNumber;
+		result.imaginaryNumber += this.imaginaryNumber * numberA.realNumber;
+		result.realNumber -= this.imaginaryNumber * numberA.imaginaryNumber;
+		return result.realNumber + " + " + result.imaginaryNumber + "i";
+	}
+	public String division(Complex numberA){
+		Complex result = new Complex();
+		
+		return "";
 	}
 }
