c686cab4ae90b49e510c19a9cb9837892d197593
Sakib Sami <s4kibs4mi@gmail.com>
2014-04-10 18:30:16 +0600

    Create FirstGui.java

---
+++ FirstGui.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class FirstGui extends JFrame{
+	public JButton sum = new JButton("Sum");
+	public JButton exit = new JButton("Exit");
+	public JLabel number1 = new JLabel("Number1 : ");
+	public JLabel number2 = new JLabel("Number2 : ");
+	public JLabel resultL = new JLabel("Result : ");
+	public JTextField textNum1 = new JTextField();
+	public JTextField textNum2 = new JTextField();
+	public JLabel result = new JLabel("");
+	
+	public FirstGui() {
+		setLayout( new GridLayout(0, 2) );
+		setTitle("GUI Test Math Sum");
+		setDefaultCloseOperation( EXIT_ON_CLOSE );
+		setSize(400, 500);
+		add(number1);
+		add(textNum1);
+		add(number2);
+		add(textNum2);
+		add(resultL);
+		add(result);
+		add(sum);
+		add(exit);
+		sum.addActionListener(new CustomActionListener());
+		show();
+	}
+	
+	public class CustomActionListener implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			int a = Integer.parseInt(textNum1.getText());
+			int b = Integer.parseInt(textNum2.getText());
+	        result.setText(" " + (a + b));
+	    }
+	}
+}
